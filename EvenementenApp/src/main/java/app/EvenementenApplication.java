779e9e13cbc4d3c18575ed2028b4a813baea8d2b
Student <samanthalourens1993@gmail.com>
2018-09-24 21:26:31 +0200

    nieuwe commit

--- EvenementenApp/src/main/java/app/EvenementenApplication.java
+++ EvenementenApp/src/main/java/app/EvenementenApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EvenementenAppApplication {
+public class EvenementenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EvenementenAppApplication.class, args);
+		SpringApplication.run(EvenementenApplication.class, args);
 	}
 }
